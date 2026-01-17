a5f7277ad8c1a3ee12a313cd76d68e7d6c4b89b9
Bulbaep <denzelogold@gmail.com>
2026-01-17 11:45:02 +0100

    Add missing constants to config_railway

--- config_railway.py
+++ config_railway.py
@@ -7,3 +7,8 @@ LICHESS_BOT_CLAUDE_USERNAME = os.environ.get('LICHESS_BOT_CLAUDE_USERNAME')
 LICHESS_BOT_GPT_USERNAME = os.environ.get('LICHESS_BOT_GPT_USERNAME')
 ANTHROPIC_API_KEY = os.environ.get('ANTHROPIC_API_KEY')
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+# Game settings
+TIME_CONTROL = "180+2"  # 3 minutes + 2 seconds increment
+COLOR = "random"  # "white", "black", or "random"
+GAME_DELAY = 15  # Seconds between games
